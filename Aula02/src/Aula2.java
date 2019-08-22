fe4983308b603f97bfab3415648a130dd1dfef1c
32214569lfq <lais.frequeiroz@gmail.com>
2019-08-22 10:23:24 -0300

    aulas

---
+++ Aula02/src/Aula2.java
@@ -0,0 +1,17 @@
+package aula02;
+public class Aula2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+     void desligar() 
+     {
+    	 if(desligar == false)
+    	 {
+    		 System.out.println("Está ligada!");
+    	 }
+    	 desligar = true
+     }
---
+++ Aula02/src/Aula3.java
@@ -0,0 +1,9 @@
+package aula02;
+public class Aula2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
